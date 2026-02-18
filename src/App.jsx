0da4c2bbf2abca604995c8751de2af0c98f62245
RiaangrimJoubert <riaangrimjoubert@gmail.com>
2026-02-18 20:39:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
-import { useMemo, useState } from "react";
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
-
+import { useState } from "react";
 import Login from "./pages/Login";
 import AppShell from "./shell/AppShell";
-import InvoiceHtml from "./pages/InvoiceHtml";
 
 export default function App() {
   const [authed, setAuthed] = useState(false);
-  const location = useLocation();
 
-  const isClientPortal = useMemo(() => {
-    return location.pathname.startsWith("/invoices");
-  }, [location.pathname]);
-
-  // Client portal (public)
-  if (isClientPortal) {
-    return (
-      <Routes>
-        <Route path="/invoices" element={<InvoiceHtml />} />
-        <Route path="/invoices/:invoiceId" element={<InvoiceHtml />} />
-        <Route path="*" element={<Navigate to="/invoices" replace />} />
-      </Routes>
-    );
-  }
-
-  // Admin app (login protected)
   if (!authed) return <Login onLogin={() => setAuthed(true)} />;
 
   return <AppShell onLogout={() => setAuthed(false)} />;
